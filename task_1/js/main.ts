5db1466569bb12253cfc12c0015c33918cd67a54
benbraikilyas <benbraikilyas9@gmail.com>
2025-09-14 00:46:48 +0100

    change about file main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,11 +1,10 @@
 interface Teacher {
-    readonly firstName: string;           // only set when initialized
-    readonly lastName: string;            // only set when initialized
-    fullTimeEmployee: boolean;             // always defined
-    yearsOfExperience?: number;             // optional
-    location: string;                       // always defined
-  
-    [key: string]: any;                     // allow any other attributes
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;  
+    [key: string]: any; 
   }
   
   const teacher3: Teacher = {
